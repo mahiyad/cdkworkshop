166e94f142f97a6c3a173fe200037765980c4b75
Yadav <2031846@cognizant.com>
2021-08-06 09:39:38 +0530

    cdk workshop code commit

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from aws_cdk import core
+
+from cdkworkshop.cdkworkshop_stack import CdkworkshopStack
+
+
+app = core.App()
+CdkworkshopStack(app, "cdkworkshop")
+#CdkWorkshopStack(app, "cdkworkshop", env={'region': 'us-west-2'})
+
+app.synth()
---
+++ cdkworkshop/cdkworkshop_stack.py
@@ -0,0 +1,18 @@
+from aws_cdk import (
+    core,
+    aws_lambda as _lambda,
+)
+
+
+class CdkworkshopStack(core.Stack):
+
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # Defines an AWS Lambda resource
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.asset('lambda'),
+            handler='hello.handler',
+        )
